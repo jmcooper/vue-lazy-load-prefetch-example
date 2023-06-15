9e24e14de797a02333e6808dde5690f0816de06a
Jim Cooper <jim@cooperplanet.com>
2023-06-15 17:52:16 -0600

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   'extends': [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'eslint:recommended',
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 0,
+    'operator-linebreak': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'never'],
+  },
 }
