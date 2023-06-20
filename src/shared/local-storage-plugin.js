5704353d3f137509ffc08c708bc28e176c8b91b5
Jim Cooper <jim@cooperplanet.com>
2023-06-20 14:57:55 -0600

    Remove plugin

--- src/shared/local-storage-plugin.js
+++
@@ -1,7 +0,0 @@
-export default function LocalStoragePlugin({ store }) {
-  store.$subscribe((mutation, state) => {
-    for (const stateKey in state) {
-      localStorage.setItem(`${mutation.storeId}-${stateKey}`, JSON.stringify(state[stateKey]))
-    }
-  })
-}
