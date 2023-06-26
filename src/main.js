6f0495dd20050a6216463b934ecb9b66c3f69b55
Jim Cooper <jim@cooperplanet.com>
2023-06-26 12:43:36 -0600

    Disable LocalStorage Plugin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import LocalStoragePlugin from './shared/local-storage-plugin'
+// import LocalStoragePlugin from './shared/local-storage-plugin'
 
 const pinia = createPinia()
-pinia.use(LocalStoragePlugin)
+// pinia.use(LocalStoragePlugin)
 
 createApp(App)
   .use(router)
